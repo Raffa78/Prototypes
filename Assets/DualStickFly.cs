08ff90d415bf051672866011bbe8249155b43531
Raffaele Liguri <raffaele.liguri@gmail.com>
2018-01-21 22:09:01 +0100

    icarus flight

--- Assets/DualStickFly.cs
+++ Assets/DualStickFly.cs
@@ -15,6 +15,13 @@ public class DualStickFly : MonoBehaviour {
 
 	public float maxForce = 1.0f;
 
+	[Header("Wing Blow Settings")]
+	public AnimationCurve forceProfile;
+	bool canBlow = true;
+	bool blowing = false;
+	float lastBlowTime;
+	float profileDuration;
+
 	float RHorizontal;
 	float RVertical;
 	float LHorizontal;
@@ -30,7 +37,8 @@ public class DualStickFly : MonoBehaviour {
 	}
 
 	void Start () {
-		
+		profileDuration = forceProfile.keys [forceProfile.length - 1].time;
+		print (profileDuration);
 	}
 	
 	// Update is called once per frame
@@ -43,8 +51,6 @@ public class DualStickFly : MonoBehaviour {
 		RTrigger = Input.GetAxis ("RTrigger");
 		LTrigger = Input.GetAxis ("LTrigger");
 
-		print (LHorizontal + " " + LVertical + " " + RTrigger + " " + LTrigger);
-
 		eyes.transform.rotation = HRotBody.transform.rotation * VRotBody.transform.rotation;
 
 
@@ -55,6 +61,11 @@ public class DualStickFly : MonoBehaviour {
 		HRotBody.AddRelativeTorque (Vector3.up * RHorizontal * maxHTorque);
 		VRotBody.AddRelativeTorque (Vector3.right * RVertical * maxVTorque);
 
+		IcarusFlight ();
+	}
+
+	void BasicFlight() {
+		
 		body.AddForce (
 			HRotBody.transform.right * LHorizontal * maxForce
 			+ HRotBody.transform.forward * LVertical * maxForce 
@@ -63,4 +74,39 @@ public class DualStickFly : MonoBehaviour {
 		);
 	}
 
+
+	void IcarusFlight() {
+
+		
+		Vector3 force = HRotBody.transform.right * LHorizontal
+		                + HRotBody.transform.forward * LVertical
+		                + Vector3.up * RTrigger
+		                - Vector3.up * LTrigger;
+
+		if (force.magnitude > 1.0f) {
+			force.Normalize ();
+		}
+
+		if (canBlow && force.magnitude > 0.1) {
+			
+			canBlow = false;
+			blowing = true;
+			lastBlowTime = Time.time;
+		}
+			
+		if (blowing) {
+			
+			float t = Time.time - lastBlowTime;
+
+			if (t > profileDuration) {
+				
+				blowing = false;
+				canBlow = true;
+
+			} else {
+
+				body.AddForce(force * maxForce * forceProfile.Evaluate(t));
+			}
+		}
+	}
 }
