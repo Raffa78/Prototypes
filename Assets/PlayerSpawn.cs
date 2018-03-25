3c6575518756a7903c48c6f338b6c44573032dd5
Raffaele Liguri <raffaele.liguri@gmail.com>
2018-03-25 23:59:51 +0200

    add

---
+++ Assets/PlayerSpawn.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawn : MonoBehaviour {
+
+	static 
+	// Use this for initialization
+	void OnJoinedRoom () {
+		
+		Vector3 position = new Vector3( Random.value * 33.5f, 0.0f, 3.0f );
+		GameObject newPlayerObject = PhotonNetwork.Instantiate( "TestPlayer", position, Quaternion.identity, 0 );
+	}
+
+}
