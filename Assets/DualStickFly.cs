36ece6f35abc833666884056f985b33ca76e43be
Raffaele Liguri <raffaele.liguri@gmail.com>
2018-04-01 00:29:43 +0200

    by batch

--- Assets/DualStickFly.cs
+++ Assets/DualStickFly.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DualStickFly : Photon.PunBehaviour, IPunObservable {
+public class DualStickFly : Photon.MonoBehaviour, IPunObservable {
 
 	Rigidbody body;
 
