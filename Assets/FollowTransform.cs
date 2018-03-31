55f7d736df351d41ddd3c0a0c18acefc2ea6e947
Raffaele Liguri <raffaele.liguri@gmail.com>
2018-04-01 00:48:37 +0200

    fds

---
+++ Assets/FollowTransform.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTransform : MonoBehaviour {
+
+	public Transform target;
+	
+	// Update is called once per frame
+	void Update () {
+		if (target == null)
+			return;
+
+		transform.position = target.transform.position;
+	}
+}
