10fb2b6de459698364d41ddd3fbcf44b67039404
Raffaele Liguri <raffaele.liguri@gmail.com>
2018-04-04 23:57:29 +0200

    by batch

--- Assets/AnimatedSword.cs
+++ Assets/AnimatedSword.cs
@@ -80,17 +80,19 @@ public class AnimatedSword : MonoBehaviour {
 		swingAlreadyHit = true;
 	}
 
+
+	int countToDeath = 3;
+
 	[PunRPC]
-	int count = 0;
 	public void SwordHit(int ID, Vector3 impulse)
 	{
 		print ("HIT!");
 		Rigidbody body = PhotonView.Find (ID).GetComponent<Rigidbody>();
 		body.AddForce (hitForce * impulse, ForceMode.Impulse);
 
-		count++;
+		countToDeath--;
 
-		if (count == 3) {
+		if (countToDeath == 0) {
 			PhotonNetwork.Destroy (GetComponentInParent<DualStickFly> ().gameObject);
 			Vector3 position = GameObject.Find("Spawn").transform.position;
 			GameObject newPlayerObject = PhotonNetwork.Instantiate( "TestPlayer", position, Quaternion.identity, 0 );
