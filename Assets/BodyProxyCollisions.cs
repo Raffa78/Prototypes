439287b792820fd8ed76b90e38abe01c8c8bf876
Raffaele Liguri <raffaele.liguri@gmail.com>
2018-04-04 23:19:00 +0200

    missings

---
+++ Assets/BodyProxyCollisions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyProxyCollisions : MonoBehaviour {
+
+	public float disablingTimeToImpulseRatio = 1;
+
+	void OnCollisionEnter(Collision collision)
+	{
+		
+	}
+}
---
+++ Assets/CollisionEnterPrint.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionEnterPrint : MonoBehaviour {
+
+	void OnCollisionEnter(Collision collision)
+	{
+		if (collision.collider.gameObject.name.Contains ("Ground"))
+			return;
+		
+	}
+}
