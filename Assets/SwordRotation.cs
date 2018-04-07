f248f3fc3c49d222d01b1d16fc8dbe6b95bf5a67
Raffaele Liguri <raffaele.liguri@gmail.com>
2018-04-07 22:25:00 +0200

    t

---
+++ Assets/SwordRotation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordRotation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.rotation = Quaternion.LookRotation(transform.parent.forward, Vector3.up);
+	}
+}
