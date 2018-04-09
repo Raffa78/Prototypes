c8be5c26b71f2a340f6895f67956b752890baff2
Raffaele Liguri <raffaele.liguri@gmail.com>
2018-04-09 22:06:45 +0200

    t

---
+++ Assets/NetInfos.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NetInfos : MonoBehaviour {
+
+	PhotonView photonView;
+	Text pingText;
+	// Use this for initialization
+	void Start () {
+		photonView = GetComponent<PhotonView>();
+
+		if (!photonView.isMine)
+		{
+			gameObject.SetActive(false);
+			return;
+		}
+
+		pingText = transform.Find("Ping").GetComponent<Text>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!photonView.isMine)
+			return;
+
+		pingText.text = PhotonNetwork.GetPing().ToString();
+	}
+}
