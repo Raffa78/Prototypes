0cb836f980069ce51bfbfef326bff7423d6646a0
Raffaele Liguri <raffaele.liguri@gmail.com>
2018-04-12 22:43:09 +0200

    t

---
+++ Assets/NetBall.cs
@@ -0,0 +1,31 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class NetBall : MonoBehaviour {
+
+	PhotonView photonView;
+	
+	// Use this for initialization
+	void Start () {
+		photonView = GetComponent<PhotonView>();
+
+		if(!photonView.isMine)
+		{
+			foreach(NetBall ball in GameObject.FindObjectsOfType<NetBall>())
+			{
+				ConfigurableJoint joint = ball.GetComponent<ConfigurableJoint>();
+				if (joint != null)
+				{
+					joint.connectedBody = GetComponent<Rigidbody>();
+				}
+			}
+		}
+		else
+		{
+			Destroy(GetComponent<ConfigurableJoint>());
+		}	
+	}
+}
