a9a7b9cfa7ac6f4d43832132b7fc5fea631e9aec
Raffaele Liguri <raffaele.liguri@gmail.com>
2018-05-18 21:05:44 +0200

    no message

--- Assets/SBBall.cs
+++ Assets/SBBall.cs
@@ -8,25 +8,44 @@ public class SBBall : MonoBehaviour {
 
 	PhotonView photonView;
 	Rigidbody myRigidbody;
+	bool isCatched;
 
 	// Use this for initialization
-	void Start () {
+	IEnumerator Start() {
 		myRigidbody = GetComponent<Rigidbody>();
 		photonView = GetComponent<PhotonView>();
 		transform.parent = null;
-		
 
 		if (!PhotonNetwork.player.IsMasterClient && photonView.isMine)
 		{
+			yield return new WaitForSeconds(0.5f);
 			Destroy(gameObject);
-			return;
-		}	
+			yield break;
+		}
 
-		if(!photonView.isMine)
+		if (!photonView.isMine)
 		{
 			myRigidbody.isKinematic = true;
 		}
-		
+
+	}
+
+	public void Update()
+	{
+		if (!photonView.isMine)
+		{
+			myRigidbody.isKinematic = true;
+			return;
+		}
+
+		if (isCatched)
+		{
+			myRigidbody.isKinematic = true;
+		}
+		else
+		{
+			myRigidbody.isKinematic = false;
+		}
 	}
 
 	public void TakeOver()
@@ -42,5 +61,15 @@ public class SBBall : MonoBehaviour {
 	public bool IsMine()
 	{
 		return photonView.isMine;
+	}
+
+	public void Catch()
+	{
+		isCatched = true;
+	}
+
+	public void Uncatch()
+	{
+		isCatched = false;
 	}
 }
