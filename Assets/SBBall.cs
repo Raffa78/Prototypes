00addcf759efc9a95e0ce1c6588d0a077833d7e9
Raffaele Liguri <raffaele.liguri@gmail.com>
2018-05-18 20:22:45 +0200

    no message

--- Assets/SBBall.cs
+++ Assets/SBBall.cs
@@ -7,16 +7,26 @@ using System.Linq;
 public class SBBall : MonoBehaviour {
 
 	PhotonView photonView;
-	
+	Rigidbody myRigidbody;
+
 	// Use this for initialization
 	void Start () {
+		myRigidbody = GetComponent<Rigidbody>();
 		photonView = GetComponent<PhotonView>();
 		transform.parent = null;
+		
 
 		if (!PhotonNetwork.player.IsMasterClient && photonView.isMine)
 		{
 			Destroy(gameObject);
+			return;
 		}	
+
+		if(!photonView.isMine)
+		{
+			myRigidbody.isKinematic = true;
+		}
+		
 	}
 
 	public void TakeOver()
