7f0a6332415a46e59144714da6e04a804b893cf5
Raffaele Liguri <raffaele.liguri@gmail.com>
2018-06-09 11:33:41 +0200

    no message

---
+++ Assets/SprintSlider.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SprintSlider : MonoBehaviour {
+
+	Slider slider;
+	SBPlayer player;
+
+	// Use this for initialization
+	void Start () {
+		slider = GetComponent<Slider>();
+		player = GetComponentInParent<SBPlayer>();
+
+		if(!player.GetComponent<PhotonView>().isMine)
+		{
+			Destroy(gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		slider.value = player.sprintPool;
+	}
+}
